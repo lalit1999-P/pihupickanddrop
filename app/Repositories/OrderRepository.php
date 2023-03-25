4efb5114e34e76bff442ddfd5508d721ad8567f9
Prajapati1999@ <prajapatilalit1999@gmail.com>
2023-03-25 13:35:19 +0530

    get all image api change

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -105,31 +105,40 @@ class OrderRepository
     public function getOrderHistory($assign_status)
     {
         // dd($assign_status);
-        $Orderlists = Order::where('driver_id', auth()->user()->id)->where('assign_status', $assign_status)->with('DriverUsers', 'Varient', 'vehicleModel', 'location', 'pickupImage', 'dropOffImage')->get()->toArray();
+        $Orderlists = Order::where('driver_id', auth()->user()->id)->where('assign_status', $assign_status)->with('DriverUsers', 'Varient', 'vehicleModel', 'location')->get()->toArray();
         $order = [];
+        $actionStatus = \Config::get('configvalue.ORDER_ACTION_STATUS');
         foreach ($Orderlists as $Orderlist) {
-            //dd($Orderlist);
-            if (empty($Orderlist['pickup_image'])) {
-                $Orderlist['pickup_image'] = [];
-            }
-            if (empty($Orderlist['drop_off_image'])) {
-                $Orderlist['drop_off_image'] = [];
-            }
+            // dd($Orderlist['id']);
+            $pickup_image = ServiceOrderImage::where('service_order_id', $Orderlist['id'])->select('image')->where('service_order_image_type', $actionStatus['PICKUP_IMAGE'])->get();
+            $Orderlist['pickup_image'] = isset($pickup_image) ? $pickup_image : [];
+            $drop_image_service_center = ServiceOrderImage::where('service_order_id', $Orderlist['id'])->select('image')->where('service_order_image_type', $actionStatus['DROP_IMAGE_SERVICE_CENTER'])->get();
+            $Orderlist['drop_image_service_center'] = isset($drop_image_service_center) ? $drop_image_service_center : [];
+            $pickup_image_service_center = ServiceOrderImage::where('service_order_id', $Orderlist['id'])->select('image')->where('service_order_image_type', $actionStatus['PICKUP_IMAGE_SERVICE_CENTER'])->get();
+            $Orderlist['pickup_image_service_center'] = isset($pickup_image_service_center) ? $pickup_image_service_center : [];
+            $drop_off_image = ServiceOrderImage::where('service_order_id', $Orderlist['id'])->select('image')->where('service_order_image_type', $actionStatus['DROP_IMAGE'])->get();
+            $Orderlist['drop_off_image'] = isset($drop_off_image) ? $drop_off_image : [];
+            $Orderlist['image_base_url'] = 'https://pihupickanddrop.com/order_img/';
             $order[] = $Orderlist;
         }
         return $order;
     }
     public function getNewOrder()
     {
-        $Orderlists = Order::where('driver_id', auth()->user()->id)->where('assign_status', 1)->with('DriverUsers', 'Varient', 'vehicleModel', 'location', 'pickupImage', 'dropOffImage')->get()->toArray();
+        $Orderlists = Order::where('driver_id', auth()->user()->id)->where('assign_status', 1)->with('DriverUsers', 'Varient', 'vehicleModel', 'location')->get()->toArray();
         $order = [];
+        $actionStatus = \Config::get('configvalue.ORDER_ACTION_STATUS');
+
         foreach ($Orderlists as $Orderlist) {
-            if (empty($Orderlist['pickup_image'])) {
-                $Orderlist['pickup_image'] = [];
-            }
-            if (empty($Orderlist['drop_off_image'])) {
-                $Orderlist['drop_off_image'] = [];
-            }
+            $pickup_image = ServiceOrderImage::where('service_order_id', $Orderlist['id'])->select('image')->where('service_order_image_type', $actionStatus['PICKUP_IMAGE'])->get();
+            $Orderlist['pickup_image'] = isset($pickup_image) ? $pickup_image : [];
+            $drop_image_service_center = ServiceOrderImage::where('service_order_id', $Orderlist['id'])->select('image')->where('service_order_image_type', $actionStatus['DROP_IMAGE_SERVICE_CENTER'])->get();
+            $Orderlist['drop_image_service_center'] = isset($drop_image_service_center) ? $drop_image_service_center : [];
+            $pickup_image_service_center = ServiceOrderImage::where('service_order_id', $Orderlist['id'])->select('image')->where('service_order_image_type', $actionStatus['PICKUP_IMAGE_SERVICE_CENTER'])->get();
+            $Orderlist['pickup_image_service_center'] = isset($pickup_image_service_center) ? $pickup_image_service_center : [];
+            $drop_off_image = ServiceOrderImage::where('service_order_id', $Orderlist['id'])->select('image')->where('service_order_image_type', $actionStatus['DROP_IMAGE'])->get();
+            $Orderlist['drop_off_image'] = isset($drop_off_image) ? $drop_off_image : [];
+            $Orderlist['image_base_url'] = 'https://pihupickanddrop.com/order_img/';
             $order[] = $Orderlist;
         }
         return $order;
@@ -138,12 +147,15 @@ class OrderRepository
     public function getOrderDetail($order_id)
     {
         $Orderlists = Order::where('driver_id', auth()->user()->id)->where("id", $order_id)->with('DriverUsers', 'Varient', 'vehicleModel', 'location', 'pickupImage', 'dropOffImage')->first()->toArray();
-        if (empty($Orderlists['pickup_image'])) {
-            $Orderlists['pickup_image'] = [];
-        }
-        if (empty($Orderlists['drop_off_image'])) {
-            $Orderlists['drop_off_image'] = [];
-        }
+        $pickup_image = ServiceOrderImage::where('service_order_id', $Orderlists->id)->select('image')->where('service_order_image_type', $actionStatus['PICKUP_IMAGE'])->get();
+        $Orderlists['pickup_image'] = isset($pickup_image) ? $pickup_image : [];
+        $drop_image_service_center = ServiceOrderImage::where('service_order_id', $Orderlists->id)->select('image')->where('service_order_image_type', $actionStatus['DROP_IMAGE_SERVICE_CENTER'])->get();
+        $Orderlists['drop_image_service_center'] = isset($drop_image_service_center) ? $drop_image_service_center : [];
+        $pickup_image_service_center = ServiceOrderImage::where('service_order_id', $Orderlists->id)->select('image')->where('service_order_image_type', $actionStatus['PICKUP_IMAGE_SERVICE_CENTER'])->get();
+        $Orderlists['pickup_image_service_center'] = isset($pickup_image_service_center) ? $pickup_image_service_center : [];
+        $drop_off_image = ServiceOrderImage::where('service_order_id', $Orderlists->id)->select('image')->where('service_order_image_type', $actionStatus['DROP_IMAGE'])->get();
+        $Orderlists['drop_off_image'] = isset($drop_off_image) ? $drop_off_image : [];
+        $Orderlists['image_base_url'] = 'https://pihupickanddrop.com/order_img/';
         return $Orderlists;
     }
 }
